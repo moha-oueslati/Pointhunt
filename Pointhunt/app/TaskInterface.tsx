8a6e6d510f851dac6fe9549bced99401e1cdba60
Matva778 <Matva778@student.liu.se>
2025-11-07 11:59:58 +0100

    TaskInterface typ fixad
    TaskInterface för guest är ganska klar, kan hända att små fixar behöver läggas till

--- Pointhunt/app/TaskInterface.tsx
+++ Pointhunt/app/TaskInterface.tsx
@@ -2,10 +2,25 @@ import { useState } from 'react'
 import { View, Button } from 'react-native';
 export default function TaskInterface(){
     const [open, setOpen] = useState(false);
+    const [num, setSwaped] = useState(0);
 
     const toggleDropDown = () => {
         setOpen((open) => !open);
     };
+    const handleChange = (num2: number) => {
+        setSwaped(num2);
+        setOpen(false);
+    };
+    const getTitle = (num: number) => {
+            switch(num){
+                case 1:
+                    return "Lag 1";
+                case 2:
+                    return "Lag 2";
+                default:
+                    return "Välj Lag";
+            }
+        };
     return(
         <>
             <View>
@@ -14,16 +29,23 @@ export default function TaskInterface(){
                 </title>
             </View>
             <View>
-                <Button onPress={setOpen((open) => !open)} className="dropbtn" title='Välj Lag'/>
-                <View id="DropdownMenu">
-                    <button className="Teambtn">Lag 1</button>
-                </View>
+                <Button onPress={() => {toggleDropDown()}} key={num} title={getTitle(num)}/>
+                {open &&(
+                    <View>
+                        {num !== 1 ?(
+                            <Button onPress={() => {handleChange(1)}} title='Lag 1'/>
+                        ) : (
+                            <Button onPress={() => {handleChange(0)}} title='Välj Lag'/>
+                        )}
+                        {num !== 2 ?(
+                            <Button onPress={() => {handleChange(2)}} title='Lag 2'/>
+                        ): (
+                            <Button onPress={() => {handleChange(0)}} title='Välj Lag'/>
+                        )}
+                    </View>
+                )}
             </View>
         </>
         
     );
-}
-
-function handleDrop(){
-
 }
