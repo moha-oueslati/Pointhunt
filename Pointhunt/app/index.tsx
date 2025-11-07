d4b1a694cbbf11f105bf94e654320a7d65e66e02
Hanmo953 <109065922+Hanmo953@users.noreply.github.com>
2025-11-07 11:03:00 +0100

    Index sidan

--- Pointhunt/app/index.tsx
+++ Pointhunt/app/index.tsx
@@ -1,19 +1,78 @@
-import { Text, View } from "react-native";
+import { Text, View, TouchableOpacity, StyleSheet } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >    <Text>Welcome to Pointhunt!</Text>
+    <View style={styles.container}>    
+    <Text style={styles.title}> Welcome to Pointhunt! </Text>
 
-        <button> Host</button>
-    <button>
-      Guest
-    </button>
+    <TouchableOpacity style={styles.greenButton}>
+      <Text style={styles.lightGreenText}> Join as Host </Text>
+    </TouchableOpacity>
+        
+    <TouchableOpacity style={styles.brownButton}>
+      <Text style={styles.lightBrownText}> Join as Guest </Text>
+    </TouchableOpacity>
     </View>
-  );
+  ); 
 }
+
+const styles = StyleSheet.create({
+container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#A8EFAB" //ljusgrönt till bakgrunden
+},
+title: {
+    fontSize: 32,
+    fontWeight: "bold",
+    color: "#614417", //mörkbrunt
+    marginBottom: 40
+},
+greenButton: {
+    backgroundColor: '#0F6B0A', //mörkgrönt
+    paddingVertical: 14,
+    borderRadius: 10,
+    marginBottom: 20,
+    width: 225,
+    alignItems: "center",
+},
+brownButton: {
+    backgroundColor: '#614417', //mörkbrunt
+    paddingVertical: 14,
+    borderRadius: 10,
+    width: 225,
+    alignItems: "center",
+},
+lightGreenText: {
+  color: '#7DFD81', //ljusgrön
+  fontSize: 18,
+},
+lightBrownText: {
+  color: '#FFD89A', //ljusbrunt eller beige
+  fontSize: 18,
+},
+darkGreenText: {
+  color: '#7DFD81',
+  fontSize: 18,
+},
+darkBrownText: {
+  color: '#614417',
+  fontSize: 18,
+},
+codeTextfield: { // Till när man lägger in kod
+  paddingVertical: 14,
+  borderRadius: 10,
+  width: 225,
+  alignItems: "center",
+},
+writeTextfield: { // För hostsidan när man skriver in titel/namn på host
+  backgroundColor: '#614417',
+  paddingVertical: 8,
+  borderRadius: 8,
+  width: 225,
+  alignItems: "center",
+}
+});
+
+
