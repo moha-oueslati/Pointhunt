828fc7a885ee3a0884895525440246025d4c8832
Hanmo953 <109065922+Hanmo953@users.noreply.github.com>
2025-11-13 13:53:52 +0100

    Update index.tsx
    Mina ändringar i index // Hani

--- Pointhunt/app/index.tsx
+++ Pointhunt/app/index.tsx
@@ -1,22 +1,29 @@
-import { Text, View, TouchableOpacity, StyleSheet } from "react-native";
 import React from "react";
+import { Text, View, TouchableOpacity, StyleSheet } from "react-native";
+import { useRouter } from "expo-router";
 
 export default function Index() {
-  
+  const router = useRouter();
 
-  // Css till texten för "Lag: 1" och "10 poäng" är att de ska sitta på motsatt sida av samma rad
-  // "Lag: 1" kommer alltså att sitta på vänster sida och "10 poäng" på höger sida
-  // Text inputen är en placeholder tills vi har en hårdkodad lista som vi kan skicka in i en funktion för "PointInfo.tsx"
   return (
     <View style={styles.container}>    
     <Text style={styles.title}> Welcome to Pointhunt! </Text>
 
-    <TouchableOpacity style={styles.greenButton}>
-      <Text style={styles.lightGreenText}> Join as Host </Text>
+    <TouchableOpacity style={styles.redButton}
+    accessibilityRole="button"
+    accessibilityLabel="Join as Host"
+    onPress={() => console.log('Join as Host pressed')}
+    >
+    <Text style={styles.lightRedText}> Join as Host </Text>
     </TouchableOpacity>
         
-    <TouchableOpacity style={styles.brownButton}>
-      <Text style={styles.lightBrownText}> Join as Guest </Text>
+    <TouchableOpacity style={styles.yellowButton}
+     accessibilityRole="button"
+     accessibilityLabel="Join as Guest"
+     onPress={() => router.push('/guest')} 
+
+    >
+      <Text style={styles.lightYellowText}> Join as Guest </Text>
     </TouchableOpacity>
     </View>
   ); 
@@ -27,57 +34,62 @@ container: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#A8EFAB" //ljusgrönt till bakgrunden
+    backgroundColor: "#151B7C" //Mörkblått till bakgrunden
 },
 title: {
     fontSize: 32,
     fontWeight: "bold",
-    color: "#614417", //mörkbrunt
+    color: "#CCB307", //Gult
     marginBottom: 40
 },
-greenButton: {
-    backgroundColor: '#0F6B0A', //mörkgrönt
+redButton: {
+    backgroundColor: '#8C070C', //mörkrött
     paddingVertical: 14,
     borderRadius: 10,
     marginBottom: 20,
     width: 225,
     alignItems: "center",
 },
-brownButton: {
-    backgroundColor: '#614417', //mörkbrunt
+yellowButton: {
+    backgroundColor: '#CCB307', //gult
     paddingVertical: 14,
     borderRadius: 10,
     width: 225,
     alignItems: "center",
 },
-lightGreenText: {
-  color: '#7DFD81', //ljusgrön
+lightRedText: {
+  color: '#FFAEB1', //rosa
   fontSize: 18,
 },
-lightBrownText: {
-  color: '#FFD89A', //ljusbrunt eller beige
+lightYellowText: {
+  color: '#FFF8C7', //ljusgult
   fontSize: 18,
 },
-darkGreenText: {
-  color: '#7DFD81',
+redText: {
+  color: '#8C070C', //mörkrött
   fontSize: 18,
 },
-darkBrownText: {
-  color: '#614417',
+yellowText: {
+  color: '#CCB307', //gult
   fontSize: 18,
 },
 codeTextfield: { // Till när man lägger in kod
   paddingVertical: 14,
+  backgroundColor: "rgba(140, 7, 12, 0.75)", //mörkrött 25% transparent
   borderRadius: 10,
   width: 225,
   alignItems: "center",
 },
 writeTextfield: { // För hostsidan när man skriver in titel/namn på host
   paddingVertical: 8,
+  backgroundColor: "rgba(140, 7, 12, 0.75)", //mörkrött 25% transparent
   borderRadius: 8,
+  borderWidth: 2,
+  borderColor: '#CCB307', //gul ram
+  marginBottom: 20,
   width: 225,
-  alignItems: "center",
 }
 });
+
 
 
