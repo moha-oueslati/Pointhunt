c6d29fb4b42ceb10f425e1efb0759aa6e8b359c3
Erix225 <94971906+Erix225@users.noreply.github.com>
2025-11-26 11:41:55 +0100

    Fixed stuff + added skeleton code for backend API

--- Pointhunt/app/waiting.tsx
+++ Pointhunt/app/waiting.tsx
@@ -1,7 +1,34 @@
-import { Text } from "react-native";
+import { Text, View, Button, StyleSheet } from "react-native";
+import { useRouter } from "expo-router";
+// import { onValue, ref } from "firebase/database";
+// import { db } from "../firebase"; 
 
-export default function Waiting(){
-    return(
-        <Text> Waiting for host to start the game...</Text>
-    )
-} 
+export default function Waiting() {
+    const router = useRouter();
+  return (
+    <View style={styles.container}>
+      <View style={styles.row}>
+        <Text style={styles.text}>Waiting for host to start the game...</Text>
+<Button title="Secret Button" onPress={() => { router.push('/TaskInterface') }} />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center", 
+    alignItems: "center", 
+    padding: 20,
+    backgroundColor: "#A8EFAB",
+  },
+  row: {
+    alignItems: "center",
+    gap: 10,
+  },
+  text: {
+    fontSize: 20,
+    textAlign: "center",
+  },
+});
