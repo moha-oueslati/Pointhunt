98ad22114ceb58a30f1512dc167b2585bf679039
Erix225 <94971906+Erix225@users.noreply.github.com>
2025-12-05 18:22:02 +0100

    added navbar

---
+++ Pointhunt/app/guest/navbar.tsx
@@ -0,0 +1,87 @@
+import React from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+import { useRouter } from 'expo-router';
+
+// Guest
+export default function Navbar() {
+  const router = useRouter();
+  return (
+    <View
+      style={{
+        flexDirection: "row",
+        alignItems: "flex-end",
+        marginBottom: 0,
+      }}
+    >
+      <View
+        style={{
+          flex: 1,
+          flexDirection: "column",
+          alignContent: "center",
+          justifyContent: "center",
+        }}
+      >
+        {/*Denna knapp kanske behöver en bild*/}
+        <TouchableOpacity onPress={() => router.push("/")}>
+          <Text>Hem</Text>
+        </TouchableOpacity>
+      </View>
+      <View
+        style={{
+          flex: 1,
+          flexDirection: "column",
+          alignContent: "center",
+          justifyContent: "center",
+        }}
+      >
+        {" "}
+        {/*Denna knapp kanske behöver en bild*/}
+        <TouchableOpacity onPress={() => router.push("/")}>
+          <Text>Tabell</Text>
+        </TouchableOpacity>
+      </View>
+      <View
+        style={{
+          flex: 1,
+          flexDirection: "column",
+          alignContent: "center",
+          justifyContent: "center",
+        }}
+      >
+        {" "}
+        {/*Denna knapp kanske behöver en bild*/}
+        <TouchableOpacity onPress={() => router.push("/TaskInterface")}>
+          <Text>Ladda upp</Text>
+        </TouchableOpacity>
+      </View>
+      <View
+        style={{
+          flex: 1,
+          flexDirection: "column",
+          alignContent: "center",
+          justifyContent: "center",
+        }}
+      >
+        {" "}
+        {/*Denna knapp kanske behöver en bild*/}
+        <TouchableOpacity onPress={() => router.push("/PointList")}>
+          <Text>Lista</Text>
+        </TouchableOpacity>
+      </View>
+      <View
+        style={{
+          flex: 1,
+          flexDirection: "column",
+          alignContent: "center",
+          justifyContent: "center",
+        }}
+      >
+        {" "}
+        {/*Denna knapp kanske behöver en bild*/}
+        <TouchableOpacity onPress={() => router.push("/Profil")}>
+          <Text>Profil</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
