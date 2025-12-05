8f1c7af062b056edf469cb110054230b6a1c011f
Erix225 <94971906+Erix225@users.noreply.github.com>
2025-12-05 18:05:06 +0100

    updated _layout

--- Pointhunt/app/_layout.tsx
+++ Pointhunt/app/_layout.tsx
@@ -1,6 +1,13 @@
-import { Stack } from "expo-router";
-import React from "react";
+import { Stack } from 'expo-router'; // HOST
+import React from 'react';
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="host" options={{ headerShown: false }} />
+      <Stack.Screen name="tasks" options={{ headerShown: false }} />
+      <Stack.Screen name="settings" options={{ headerShown: false }} />
+    </Stack>
+  );
 }
